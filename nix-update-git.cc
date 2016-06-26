2632271f21fd99a478d60e5c0eb5eb9970df9cc2
David Grayson <davidegrayson@gmail.com>
2016-06-26 02:44:40 -0700

    argument parsing

--- nix-update-git.cc
+++ nix-update-git.cc
@@ -2,6 +2,23 @@
 
 // TODO: add support for --deepClone and other options to fetchGit/nix-prefetch-git
 
+const char * help =
+    "Usage: nix-update-git [OPTION]... NIXFILE\n"
+    "Updates calls to fetchgit in the NIXFILE to fetch latest upstream version.\n"
+    "\n"
+    "Options:\n"
+    "  -h, --help    Show this help screen\n"
+    // TODO: "  --version     Show version number\n"
+    "  -q, --quiet   Suppress non-error output\n";
+
+struct NixUpdateGitOptions
+{
+    bool showHelp = false;
+    bool showVersion = false;
+    bool quiet = false;
+    std::string path;
+};
+
 struct FetchGitApp
 {
     const nix::ExprApp * app;
@@ -87,16 +104,60 @@ std::vector<StringReplacement> getStringReplacements(const FetchGitApp & app)
     return r;
 }
 
-int mainWithExceptions(int argc, char ** argv)
+NixUpdateGitOptions parseArgs(int argc, char ** argv)
 {
-    // TODO: don't hardcode these options
-    bool quiet = false;
-    std::string path = "/home/david/nixpkgs/pkgs/development/tools/build-managers/lazy/default.nix";
+    NixUpdateGitOptions options;
 
+    if (argc == 1)
+    {
+        options.showHelp = true;
+        return options;
+    }
+
+    nix::parseCmdLine(argc, argv, [&](nix::Strings::iterator & arg,
+            const nix::Strings::iterator & end)
+    {
+        if (*arg == "--help" || *arg == "-h")
+        {
+            options.showHelp = true;
+        }
+        else if (*arg == "--version")
+        {
+            options.showVersion = true;
+        }
+        else if (*arg == "--quiet" || *arg == "-q")
+        {
+            options.quiet = true;
+        }
+        else if (*arg != "" && arg->at(0) == '-')
+        {
+            return false;
+        }
+        else
+        {
+            if (!options.path.empty())
+            {
+                throw std::runtime_error("Only one file path argument is allowed.");
+            }
+            options.path = *arg;
+        }
+        return true;
+    });
+
+    if (options.path.empty() && !options.showHelp && !options.showVersion)
+    {
+        throw std::runtime_error("No files were specified.");
+    }
+
+    return options;
+}
+
+int nixUpdateGit(const NixUpdateGitOptions & options)
+{
     // Open the .nix file and parse it.
     nix::Strings searchPath;
     nix::EvalState state(searchPath);
-    nix::Expr * mainExpr = state.parseExprFromFile(path);
+    nix::Expr * mainExpr = state.parseExprFromFile(options.path);
 
     // Traverse the parsed representation of the file and gather
     // information about all calls (applications) of fetchgit.
@@ -111,7 +172,7 @@ int mainWithExceptions(int argc, char ** argv)
     // Get updated info about the upstream repository.  (Requires internet access.)
     for (FetchGitApp & fga : fetchGitApps)
     {
-        getLatestGitInfo(fga, state, quiet);
+        getLatestGitInfo(fga, state, options.quiet);
     }
 
     // Get the info about what replacements need to be made in the file.
@@ -130,21 +191,39 @@ int mainWithExceptions(int argc, char ** argv)
     // Update the .nix file if needed.
     if (replacements.size() > 0)
     {
-        performReplacements(path, replacements);
-        if (!quiet)
+        performReplacements(options.path, replacements);
+        if (!options.quiet)
         {
-            std::cerr << "Updated: " << path << std::endl;
+            std::cerr << "Updated: " << options.path << std::endl;
         }
     }
     else
     {
-        if (!quiet)
+        if (!options.quiet)
         {
-            std::cerr << "Already up-to-date: " << path << std::endl;
+            std::cerr << "Already up-to-date: " << options.path << std::endl;
         }
     }
 
     return 0;
+}
+
+int mainWithExceptions(int argc, char ** argv)
+{
+    NixUpdateGitOptions options = parseArgs(argc, argv);
+    if (options.showHelp)
+    {
+        std::cout << help;
+        return 0;
+    }
+
+    if (options.showVersion)
+    {
+        // TODO: show version
+        return 0;
+    }
+
+    return nixUpdateGit(options);
 }
 
 int main(int argc, char ** argv)
